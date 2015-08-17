995f356ea9f0a8480522d3447b5575df68306759
laixuefei <laixuefei@huoyunren.com>
2015-08-17 11:07:22 +0800

    Initial Commit

---
+++ XQGradient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XQGradient
+//
+//  Created by Candy Love on 15/8/17.
+//  Copyright (c) 2015年 XQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XQGradient/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XQGradient
+//
+//  Created by Candy Love on 15/8/17.
+//  Copyright (c) 2015年 XQ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
